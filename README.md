330050de1fa206c31c45a0cf4f8770ce65320058
Henil Patel <henilp1508@gmail.com>
2024-10-01 08:19:50 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+
+<h1 align="center"> Netflix Clone 🎬</h1>
+
+![Demo App](/frontend/public/screenshot-preview.png)
+
+
+About This Course:
+
+-   ⚛️ Tech Stack: React.js, Node.js, Express.js, MongoDB, Tailwind
+-   🔐 Authentication with JWT
+-   📱 Responsive UI
+
+
+### Setup .env file
+
+```bash
+PORT=5000
+MONGO_URI=your_mongo_uri
+NODE_ENV=development
+JWT_SECRET=your_jwt_secre
+
+```
+
+### Run this app locally
+
+
+### Start the app
+
+```shell
+npm run dev
+```
+
+
+
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import Footer from "./components/footer";
+import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authUser";
+import { useEffect } from "react";
+function App() {
+  const { user, isCheckingAuth, authCheck } = useAuthStore();
+  useEffect(() => {
+		authCheck();
+	}, [authCheck]);
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={!user ? <LoginPage/> : <Navigate to={"/"}/>}/>
+        <Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to={"/"}/>} />
+      </Routes>
+      <Footer/>
+      <Toaster/>
+    </>
+  )
+}
+
+export default App;
